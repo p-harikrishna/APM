91dbd5bcb0ef8e537ab67011a290f8bff6a45de7
Harikrishna Pasumarthi <p_harikrishna@yahoo.com>
2018-06-29 20:33:36 -0500

    APM Project

---
+++ src/app/products/product-detail.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  //selector: 'pm-product-detail', - We are going to display the view as part of the routing and hence selector will be removed.
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent implements OnInit {
+
+  pageTitle: string = 'Product Detail';
+  product: IProduct;
+
+  constructor(private _route: ActivatedRoute,
+               private _router: Router) { }
+
+  ngOnInit()
+  {
+    let id = +this._route.snapshot.paramMap.get('id'); //same parameter name as defined in route configuration.
+                    // the + is a java script convention to convert the parameter string to a numeric id.
+    this.pageTitle += `: ${id}`;
+    this.product = {
+      "productId": id,
+      "productName": "Leaf Rake",
+      "productCode": "GDN-0011",
+      "releaseDate": "March 19, 2016",
+      "description": "Leaf rake with 48-inch wooden handle.",
+      "price": 19.95,
+      "starRating": 3.2,
+      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+    }
+  }
+
+  onBack(): void {
+    this._router.navigate(['/products']);
+  }
+}
---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
+import { ProductService } from './product.service';
+
+@Component({
+  //selector: 'pm-products',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  filteredProducts: IProduct[];
+  products: IProduct[] = []
+  pageTitle: string = 'Product List';
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false;
+  errorMessage: string;
+  securityKey: string;
+
+  ngOnInit(): void
+  {
+    this._productService.getProducts()
+      .subscribe(products =>
+      {
+        this.products = products;
+        this.filteredProducts = this.products;
+      },
+        error => this.errorMessage = <any>error);
+
+    /*
+    this._productService.getSecurityKey()
+      .subscribe(securityKey =>
+      {
+        this.securityKey = securityKey;
+      },
+        error => this.errorMessage = <any>error);
+    */
+    //console.log('In onInit');
+  }
+
+
+
+  _listFilter: string;
+  get listFilter(): string {
+    return this._listFilter;
+  }
+
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+  }
+
+  
+  constructor(private _productService: ProductService)
+  {
+  }
+
+  onRatingClicked(message: string): void
+  {
+    this.pageTitle = 'Product List: ' + message;
+  }
+  performFilter(filterBy: string): IProduct[]
+  {
+    filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) => product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+  }
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
+}
